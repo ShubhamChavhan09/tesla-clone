ed9943b198dd95f64b52f30a0fc506c165a80efc
Shubham <shubh.chavhan00@gmail.com>
2022-01-11 18:39:41 +0530

    added home component to the App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header";
+import Home from "./components/Home";
 import GlobalStyle from "./globalStyles";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <GlobalStyle />
       <Header />
+      <Home />
     </div>
   );
 }
